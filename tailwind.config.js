7f445e15c8791ef0ae4c9c89b2ae2f4f9eb2fab8
khoirul2009 <khoirulafwan20@gmail.com>
2023-07-18 12:51:48 +0700

    Portofolio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0891b2",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+    },
+  },
+  plugins: [],
+};
